b950d6edd580e2ddc32e09602027506a49e2231a
omergamel2023-alt <omergamel2023@gmail.com>
2025-09-14 01:22:36 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,15 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.AIzaSyBqaswnE2ISDonSOw4U79afumqnIn28Iz4),
-        'process.env.AIzaSyBqaswnE2ISDonSOw4U79afumqnIn28Iz4': JSON.stringify(env.AIzaSyBqaswnE2ISDonSOw4U79afumqnIn28Iz4)
+export default defineConfig(() => {
+  return {
+    define: {
+      'process.env.API_KEY': JSON.stringify("AIzaSyBqaswnE2ISDonSOw4U79afumqnIn28Iz4"),
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+    },
+  };
 });
