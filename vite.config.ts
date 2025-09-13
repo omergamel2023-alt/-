95b314ec6e775944ee7e64a71103028fd0e6fa1b
omergamel2023-alt <omergamel2023@gmail.com>
2025-09-14 01:23:54 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig(() => {
   return {
     define: {
-      'process.env.API_KEY': JSON.stringify("AIzaSyBqaswnE2ISDonSOw4U79afumqnIn28Iz4"),
+      'process.env.GEMINI_API_KEY': JSON.stringify("AIzaSyBqaswnE2ISDonSOw4U79afumqnIn28Iz4"),
     },
     resolve: {
       alias: {
